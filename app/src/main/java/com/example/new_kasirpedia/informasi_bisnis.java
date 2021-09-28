e494626b8b5ef49f0bb87a32213cc95f2b29c4b6
Agistira <agiistira@gmail.com>
2021-09-28 14:37:08 +0700

    Initial Project

---
+++ app/src/main/java/com/example/new_kasirpedia/informasi_bisnis.java
@@ -0,0 +1,14 @@
+package com.example.new_kasirpedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class informasi_bisnis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi_bisnis);
+    }
+}
